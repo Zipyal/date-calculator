6b265e120085c76d5cdf82a8280456feba8f33be
fletch <anokhin.k404@gmail.com>
2026-09-13 01:13:51 +0300

    фикс вывода расчета даты

--- app/Services/DateCalculatorService.php
+++ app/Services/DateCalculatorService.php
@@ -2,13 +2,45 @@
 
 namespace App\Services;
 
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+
 class DateCalculatorService
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public function calculate(Carbon $start, Carbon $end): array
     {
-        //
+        $totalDays = $start->diffInDays($end);
+        $totalHours = $start->diffInHours($end);
+        $totalMinutes = $start->diffInMinutes($end);
+        $totalSeconds = $start->diffInSeconds($end);
+
+        // Создаем период для подсчета рабочих дней
+        $period = CarbonPeriod::create($start, $end);
+
+        $weekdays = 0;
+        $weekends = 0;
+
+        foreach ($period as $date) {
+            if ($date->isWeekend()) {
+                $weekends++;
+            } else {
+                $weekdays++;
+            }
+        }
+
+        return [
+            'total_days' => $totalDays,
+            'weeks' => floor($totalDays / 7),
+            'remaining_days' => $totalDays % 7,
+            'months' => $start->diffInMonths($end),
+            'years' => $start->diffInYears($end),
+            'hours' => $totalHours,
+            'minutes' => $totalMinutes,
+            'seconds' => $totalSeconds,
+            'weekdays' => $weekdays,
+            'weekends' => $weekends,
+            'start_date_formatted' => $start->format('d.m.Y'),
+            'end_date_formatted' => $end->format('d.m.Y'),
+        ];
     }
 }
